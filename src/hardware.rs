32edffb98e52b1e4f59bb7a683c97817f5df74d0
Stener Nordnes <stener.nordnes@gmail.com>
2026-02-20 13:24:28 +0100

    Fixed unresponsive buttons

--- src/hardware.rs
+++ src/hardware.rs
@@ -1,3 +1,4 @@
+use crate::types::{event::Event, orders::OrderType};
 use crossbeam_channel::Sender;
 use driver_rust::elevio::elev::Elevator;
 use std::{
@@ -5,21 +6,24 @@ use std::{
     time::Duration,
 };
 
-use crate::types::{event::Event, orders::OrderType};
-
 pub fn spawn_button_poller(elevator: &Elevator, channel_sender: Sender<Event>) {
     let elevator_handler = elevator.clone();
     let num_floors = elevator_handler.num_floors;
 
     thread::spawn(move || {
-        let mut prev_val: Option<(u8, OrderType)> = None;
+        let mut prev_states = [[false; 3]; 4];
+
+        let button_types = [OrderType::Cab, OrderType::HallDown, OrderType::HallUp];
 
         loop {
             for floor in 0..num_floors {
-                for call in [OrderType::Cab, OrderType::HallDown, OrderType::HallUp] {
-                    let pressed = elevator_handler.call_button(floor, call.into());
+                for (btn_idx, &call) in button_types.iter().enumerate() {
+                    let is_pressed = elevator_handler.call_button(floor, call.into());
+                    let was_pressed = prev_states[floor as usize][btn_idx];
 
-                    if !pressed || prev_val == Some((floor, call)) {
+                    prev_states[floor as usize][btn_idx] = is_pressed;
+
+                    if !is_pressed || was_pressed {
                         continue;
                     }
 
@@ -30,7 +34,6 @@ pub fn spawn_button_poller(elevator: &Elevator, channel_sender: Sender<Event>) {
                         // Close thread if channel is down
                         return;
                     }
-                    prev_val = Some((floor, call));
                 }
             }
             sleep(Duration::from_millis(20));
@@ -42,30 +45,25 @@ pub fn spawn_floor_poller(elevator: &Elevator, channel_sender: Sender<Event>) {
     let elevator_handler = elevator.clone();
 
     thread::spawn(move || {
-        let mut prev_val: Option<u8> = None;
+        let mut prev_floor: Option<u8> = None;
 
         loop {
             let current_floor = elevator_handler.floor_sensor();
 
-            match current_floor {
-                Some(current_floor) => {
-                    if current_floor == u8::MAX || Some(current_floor) == prev_val {
-                        continue;
-                    }
-
-                    if channel_sender
-                        .send(Event::FloorReached(current_floor))
-                        .is_err()
-                    {
+            if current_floor != prev_floor {
+                if let Some(floor) = current_floor
+                    && floor != u8::MAX
+                {
+                    let res = channel_sender.send(Event::FloorReached(floor));
+                    if res.is_err() {
                         // Close thread if channel is down
                         break;
                     }
-                    prev_val = Some(current_floor);
                 }
-                None => {
-                    // eprintln!("floor_sensor error");
-                }
+
+                prev_floor = current_floor;
             }
+
             sleep(Duration::from_millis(20));
         }
     });
